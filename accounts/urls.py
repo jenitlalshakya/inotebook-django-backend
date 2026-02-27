79dbacc91c0f952fbffddbb2eeea9d4323b6bb2b
Jenit Lal Shakya <jenit9408@gmail.com>
2026-02-27 19:49:59 +0545

    Changed '_' to '-' in url link

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from .views import signup, login, delete_account, change_password
 urlpatterns = [
     path('auth/signup/', signup, name='signup'),
     path('auth/login/', login, name='login'),
-    path('auth/delete_account/', delete_account, name='delete_account'),
-    path('auth/change_password/', change_password, name='change_password'),
+    path('auth/delete-account/', delete_account, name='delete-account'),
+    path('auth/change-password/', change_password, name='change-password'),
 ]
